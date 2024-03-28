2179fa4027ce00b3f9492af47d5f7e8e79c1a4e6
Carlos Henrique Santos Silveira <109424074+ByteCarlos@users.noreply.github.com>
2024-03-27 23:50:50 -0300

    feat/login-form

---
+++ src/view/layout/LoginForm/LoginForm.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import './style.css';
+
+const LoginForm = (props) => {
+
+    const onSubmit = () => {
+        console.log('submit...')
+    }
+
+    return (
+        <div className="login-form-container">
+            <form className="login-form" onSubmit={onSubmit} data-form>
+                <h4 className="form-title">Entrar</h4>
+                <div className="form-control">
+                    <input className="email-fieldset" type="text" name="email" placeholder="Email ou número de telefone"/>
+                </div>
+                <div className="form-control">
+                    <input className="password-fieldset" type="text" name="password" placeholder="Senha" />
+                </div>
+                <div className="button-submit-container">
+                    <button className="button-submit" data-button-submit>Entrar</button>
+                </div>
+                <div className="forgot-password-container">
+                    <span>Esqueceu a senha?</span>
+                </div>
+                <div className="remember-me-container">
+                    <input type="checkbox" name="remember" id="remember" />
+                    <span>Lembre-se de mim</span>
+                </div>
+                <div className="sign-up-container">
+                    <span className="new-here-text">Novo por aqui?</span>
+                    <span className="sign-up-link">Cadastre-se</span>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default LoginForm;
--- src/view/pages/Login/Login.jsx
+++ src/view/pages/Login/Login.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-import backgroundLogin from '../../../assets/img/login-background.svg';
+import LoginForm from "../../layout/LoginForm/LoginForm";
 import './style.css'
 
 const Login = (props) => {
     return (
         <div className="login-container">
             <h4 className="page-title">ATOS CAPITAL</h4>
+            <LoginForm />
         </div>
     );
 }
