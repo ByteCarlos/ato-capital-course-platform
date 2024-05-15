e4a91c75645745ab6836ace3c901829075bf551f
Carlos Henrique Santos Silveira <109424074+ByteCarlos@users.noreply.github.com>
2024-05-15 14:52:59 -0300

    wip: screen forgot

--- src/view/pages/ForgotPassword/ForgotPassword.jsx
+++ src/view/pages/ForgotPassword/ForgotPassword.jsx
@@ -1,8 +1,20 @@
 import React from "react";
+import Footer from "../../layout/Footer/Footer";
+import { Link } from "react-router-dom";
+import ForgotPasswordForm from "../../layout/ForgotPasswordForm/ForgotPasswordForm";
+
+import './style.css';
 
 const ForgotPassword = (props) => {
     return (
-        <div className="forgot-password-contanier"></div>
+        <div className="forgot-password-container">
+            <header>
+                <h4 className="page-title">ATOS CAPITAL</h4>
+                <Link to="/login">Entrar</Link>
+            </header>
+            <ForgotPasswordForm />
+            <Footer />
+        </div>
     );
 }
 
